cf72f3a9fb9fd2abc8401c00b1ce852c49b5da72
jlmunozpb <jlmunozpb@gmail.com>
2025-08-31 15:50:29 -0500

    main

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { connectDB } from 'src/database/connectDB'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
